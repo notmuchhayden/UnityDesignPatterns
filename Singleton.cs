1e96c062e75201e7434f617aa8d5dd3f14e69dbc
seaeast2 <seaeast2@gmail.com>
2023-10-09 10:41:59 +0900

    singleton 패턴

---
+++ Singleton.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+namespace Patterns.Singleton
+{
+    public class Singleton<T> where T : Component, new()
+    {
+        private static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindObjectOfType<T>();
+                    if (_instance == null)
+                    {
+                        GameObject obj = new GameObject();
+                        obj.name = typeof(T).Name;
+                        _instance = obj.AddComponent<T>();
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        public virtual void Awake()
+        {
+            if (_instance == null)
+            {
+                _instance = this as T;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
